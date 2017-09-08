e78cde3f3be74c04da4ac53ce970533e88be66e4
Ning Han <hanning277@163.com>
2017-09-08 10:10:27 +0800

    Add new class

---
+++ gittest/gittest/NewClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittest
+{
+    class NewClass
+    {
+        public string Name { get; set; }
+        public void Fun()
+        { }
+    }
+}
